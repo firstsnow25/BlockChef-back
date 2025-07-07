c982a7cd53d74067447c0fed44de5660bca0e15f
hyyyh0x <hyyyh0x@gmail.com>
2025-07-08 00:54:24 +0900

    refactor: validation, delete redis

--- src/main/java/com/firstsnow/blockchef/dto/email/EmailRequest.java
+++ src/main/java/com/firstsnow/blockchef/dto/email/EmailRequest.java
@@ -1,10 +1,14 @@
 package com.firstsnow.blockchef.dto.email;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.Setter;
 
 // 이메일 전송 요청
 @Getter
 public class EmailRequest {
+
+    @Email(message = "올바르지 않은 형식입니다.")
+    @NotBlank(message = "이메일은 필수 항목입니다.")
     private String email;
 }
--- src/main/java/com/firstsnow/blockchef/dto/email/VerifyCodeRequest.java
+++ src/main/java/com/firstsnow/blockchef/dto/email/VerifyCodeRequest.java
@@ -1,10 +1,16 @@
 package com.firstsnow.blockchef.dto.email;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class VerifyCodeRequest {
+
+    @Email(message = "올바르지 않은 형식입니다.")
+    @NotBlank(message = "이메일은 필수 항목입니다.")
     private String email;
+    @NotNull(message = "인증번호는 필수 항목입니다.")
     private String code;
 }
