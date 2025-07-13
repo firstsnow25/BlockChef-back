3b2fa18e888a0a00b51e6859a60b984dccb65307
hyyyh0x <hyyyh0x@gmail.com>
2025-07-13 17:15:43 +0900

    refactor: valid annotation

--- src/main/java/com/firstsnow/blockchef/controller/AuthController.java
+++ src/main/java/com/firstsnow/blockchef/controller/AuthController.java
@@ -9,6 +9,7 @@ import com.firstsnow.blockchef.service.EmailService;
 import com.firstsnow.blockchef.service.LoginService;
 import com.firstsnow.blockchef.service.UserService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -31,7 +32,7 @@ public class AuthController {
 
     // 이메일 인증코드 전송
     @PostMapping("/email/send-code")
-    public ResponseEntity<String> sendCode(@RequestBody EmailRequest request) {
+    public ResponseEntity<String> sendCode(@RequestBody @Valid EmailRequest request) {
         emailService.sendVerificationCode(request.getEmail());
         return ResponseEntity.ok("인증번호가 전송되었습니다.");
     }
@@ -45,21 +46,21 @@ public class AuthController {
 
     // 회원가입
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody SignupRequest request) {
+    public ResponseEntity<String> signup(@RequestBody @Valid SignupRequest request) {
         userService.signup(request);
         return ResponseEntity.ok("회원가입이 성공적으로 완료되었습니다.");
     }
 
     // 로그인
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<String> login(@RequestBody @Valid LoginRequest request) {
         String token = loginService.login(request);
         return ResponseEntity.ok(token); // 토큰을 응답 본문으로 전달
     }
 
     // 비밀번호 재설정
     @PostMapping("/reset-password")
-    public ResponseEntity<String> resetPassword(@RequestBody PasswordChangeRequest request) {
+    public ResponseEntity<String> resetPassword(@RequestBody @Valid PasswordChangeRequest request) {
         userService.resetPasswordByEmail(request);
         return ResponseEntity.ok("비밀번호가 성공적으로 변경되었습니다.");
     }
