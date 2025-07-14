5c4ae9e062154f8803c80fc7189d5409ebf1ed3f
hyyyh0x <hyyyh0x@gmail.com>
2025-07-14 20:49:11 +0900

    feat: swagger operation

--- src/main/java/com/firstsnow/blockchef/controller/AuthController.java
+++ src/main/java/com/firstsnow/blockchef/controller/AuthController.java
@@ -8,9 +8,13 @@ import com.firstsnow.blockchef.dto.signup.SignupRequest;
 import com.firstsnow.blockchef.service.EmailService;
 import com.firstsnow.blockchef.service.LoginService;
 import com.firstsnow.blockchef.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+
 import jakarta.validation.Valid;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,21 +31,24 @@ public class AuthController {
     private final UserService userService;
     private final LoginService loginService;
 
-    // 회원가입용 이메일 인증코드 전송
+    // 이메일 중복 검사 후 인증코드 전송
+    @Operation(summary = "회원가입 이메일 인증코드 전송", description = "회원가입할 때 이메일 중복을 검사한 후 인증코드를 전송한다.")
     @PostMapping("/email/signup/send-code")
-    public ResponseEntity<String> sendSignupVerificationCode(@RequestBody @Valid EmailRequest request) {
+    public ResponseEntity<String> sendCodeCheck(@RequestBody @Valid EmailRequest request) {
         emailService.sendSignupVerificationCode(request.getEmail());
         return ResponseEntity.ok("인증번호가 전송되었습니다.");
     }
 
-    // 비밀번호 재설정용 이메일 인증코드 전송
+    // 이메일 중복 검사 없이 인증코드 전송
+    @Operation(summary = "비밀번호 재설정 이메일 인증코드 전송", description = "비밀번호 재설정할 때 이메일 중복을 검사한 후 인증코드를 전송한다.")
     @PostMapping("/email/reset-password/send-code")
-    public ResponseEntity<String> sendResetPasswordVerificationCode(@RequestBody @Valid EmailRequest request) {
-        emailService.sendResetPasswordVerificationCode(request.getEmail());
+    public ResponseEntity<String> sendCode(@RequestBody @Valid EmailRequest request) {
+        emailService.sendVerificationCode(request.getEmail());
         return ResponseEntity.ok("인증번호가 전송되었습니다.");
     }
 
     // 이메일 인증코드 검증
+    @Operation(summary = "이메일 인증코드 확인", description = "이메일 인증코드를 확인한다.")
     @PostMapping("/email/verify-code")
     public ResponseEntity<String> verifyCode(@RequestBody @Valid VerifyCodeRequest request) {
         emailService.verifyCodeOrThrow(request.getEmail(), request.getCode());
@@ -49,6 +56,7 @@ public class AuthController {
     }
 
     // 회원가입
+    @Operation(summary = "회원가입", description = "회원가입을 한다.")
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody @Valid SignupRequest request) {
         userService.signup(request);
@@ -56,6 +64,7 @@ public class AuthController {
     }
 
     // 로그인
+    @Operation(summary = "로그인", description = "로그인을 한다.")
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody @Valid LoginRequest request) {
         String token = loginService.login(request);
@@ -63,6 +72,7 @@ public class AuthController {
     }
 
     // 비밀번호 재설정
+    @Operation(summary = "비밀번호 재설정", description = "비밀번호를 재설정한다.")
     @PostMapping("/reset-password")
     public ResponseEntity<String> resetPassword(@RequestBody @Valid PasswordChangeRequest request) {
         userService.resetPasswordByEmail(request);
